813b9be836510e8248748647ce3f48dea9810b05
Jacob Evans <cloud887@gmail.com>
2020-10-25 17:45:57 -0500

    eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,6 @@ module.exports = {
         background_color: `#fff44f`,
         theme_color: `#fff44f`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
