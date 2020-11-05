c051f9b6e2726c2cfd7ab302d626b064b4ed89cd
Jacob Evans <cloud887@gmail.com>
2020-11-04 22:06:36 -0600

    Config settings working

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,13 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
-import Image from './image'
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+import React from "react";
+import Image from "./image";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: 'linear-gradient(to right, #f7971e, #ffd200)',
+      background: `linear-gradient(to right, #f7971e, #ffd200)`,
       marginBottom: `1.45rem`,
-
     }}
   >
     <div
@@ -18,20 +17,19 @@ const Header = ({ siteTitle }) => (
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0}}>
-     
+      <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            display: 'grid',
+            display: `grid`,
 
-    gridGap: 10,
-    gridTemplateColumns: 'repeat(auto-fill, minmax(200px,1fr))',
+            gridGap: 10,
+            gridTemplateColumns: `repeat(auto-fill, minmax(200px,1fr))`,
             color: `black`,
             textDecoration: `none`,
           }}
         >
-          <div style={{width: '5vw'}}>
+          <div style={{ width: `5vw` }}>
             <Image />
           </div>
           {siteTitle}
@@ -39,14 +37,14 @@ const Header = ({ siteTitle }) => (
       </h1>
     </div>
   </header>
-)
+);
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
+};
 
 Header.defaultProps = {
   siteTitle: ``,
-}
+};
 
-export default Header
+export default Header;
